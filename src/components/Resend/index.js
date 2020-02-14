4453a36bbbbe7391ebd34305010e0e12ba7c4f2b
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-02-14 15:46:38 +0200

    resend form

--- src/components/Resend/index.js
+++ src/components/Resend/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/common';
 
 import './styles.scss';
 
 const Resend = () => {
+  const [ email, setEmail ] = useState('');
+  const [ isEmailValid, setIsEmailValid ] = useState(true);
+  const [ isVisible, setIsVisible ] = useState(true);
+
+  const inputHandler = (e) => {
+    setEmail(e.target.value);
+    setIsEmailValid(validateEmail(e.target.value));
+  };
+
+  const closeHandler = (e) => {
+    e.preventDefault();
+    setIsVisible(!isVisible);
+  };
+
+  let containerClassName = isVisible? 'resend_container' : 'resend_container resend_container_hidden';
+  let inputClassName = isEmailValid ? 'resend_input' : 'resend_input resend_input_error';
+
   return (
-    <div className="resend_container">
-      <a className="resend_close" href="/"></a>
+    <div className={containerClassName}>
+      <a 
+        className='resend_close'
+        href="/"
+        onClick={closeHandler}
+      >{""}</a>
       <p className="resend_text">Enter email if you wish to resend the meal plan link</p>
       <form className="resend_form" action="">
-        <input className="resend_input" type="text" placeholder="E-mail" />
+        <input 
+          className={inputClassName} 
+          type="text" 
+          placeholder="E-mail" 
+          value={email}
+          onChange={inputHandler}
+        />
         <button className="resend_button">Resend</button>
       </form>
     </div>
